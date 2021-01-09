5e4a65fc124d9c0b1fea4a53adca08579b507d8a
Tim Fischer <Fischer_Tim@gmx.de>
2021-01-09 15:40:37 +0100

    change date format

--- SneakerApi/database/schema/chatMessageSchema.js
+++ SneakerApi/database/schema/chatMessageSchema.js
@@ -1,9 +1,22 @@
 const mongoose = require("mongoose");
 
+function formatDate(){
+    const date =new Date(Date.now())
+    console.log(date)
+    let d = date.getDay()
+    let m = date.getMonth()+1
+    let y = date.getFullYear()
+
+    let lh = date.getHours()
+    let tm = date.getMinutes()
+    const iwas=[d,m,y].join(".")+" "+[lh,tm].join(":")
+    console.log(iwas)
+    return
+}
 
 module.exports = new mongoose.Schema({
     _senderName: {type: String, required: true},
     _message: {type: String, required: true},
     _chatId: {type: String, required: true},
-    _created: {type: Date, default: Date.now, required: true},
+    _created: {type: String, default: formatDate(Date.now), required: true},
 });
